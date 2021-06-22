6a480abb1c5c114a640ae585e660f9ac9bc712e2
Otshepeng <gasatlalang@gmail.com>
2021-06-22 18:48:04 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,94 @@
+import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent  {
+  title = 'excel-export';
+  fileName= 'ClaimViewer.xlsx';
+
+
+
+  exportexcel(): void
+  {
+    /* pass here the table id */
+    let element = document.getElementById('excel-table');
+    const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+ 
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+ 
+    /* save to file */  
+    XLSX.writeFile(wb, this.fileName);
+ 
+  }
+
+  userList = [
+
+    {
+    
+    "id": 1,
+    
+    "name": "Leanne Graham",
+    
+    "username": "Bret",
+    
+    "email": "Sincere@april.biz"
+    
+    },
+    
+    {
+    
+    "id": 2,
+    
+    "name": "Ervin Howell",
+    
+    "username": "Antonette",
+    
+    "email": "Shanna@melissa.tv"
+    
+    },
+    
+    {
+    
+    "id": 3,
+    
+    "name": "Clementine Bauch",
+    
+    "username": "Samantha",
+    
+    "email": "Nathan@yesenia.net"
+    
+    },
+    
+    {
+    
+    "id": 4,
+    
+    "name": "Patricia Lebsack",
+    
+    "username": "Karianne",
+    
+    "email": "Julianne.OConner@kory.org"
+    
+    },
+    
+    {
+    
+    "id": 5,
+    
+    "name": "Chelsey Dietrich",
+    
+    "username": "Kamren",
+    
+    "email": "Lucio_Hettinger@annie.ca"
+    
+    }
+    
+    ]
+
+}
